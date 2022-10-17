cf5b02508d25addc49299b2cb3373f1114a105c3
Winston Langat <winston.langat@student.moringaschool.com>
2022-10-17 12:02:51 +0300

    Running codes

---
+++ app.rb
@@ -0,0 +1,14 @@
+puts "Hello World!\n"
+ # puts "Hello world!"
+ # puts "Hello world!"
+ # puts "Hello world!"
+
+ print "Pass this test, please."
+ # print "Hello world!"
+ # print "Hello world!"
+ # print "Hello world!"
+
+ # puts [1, 2, 3]
+p [1, 2, 3]
+
+ # pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
